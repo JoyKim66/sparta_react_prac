dbddb403714d5c1ca79386b580eabc638583583c
JoyKim66 <masscom6051@naver.com>
2022-05-28 01:49:31 +0900

    버킷리스트 디테일 페이지에 삭제버튼 추가

--- src/Detail.js
+++ src/Detail.js
@@ -4,17 +4,29 @@ import React from "react";
 // 라우터 훅을 불러옵니다.
 import {useParams} from "react-router-dom";
 // redux hook을 불러옵니다.
-import { useSelector } from "react-redux";
+import {useHistory} from "react-router-dom";
+import { useSelector,useDispatch } from "react-redux";
+
+import {deleteBucket} from "./redux/modules/bucket";
 
 const Detail = (props) => {
+    
   // 스토어에서 상태값 가져오기
-  const bucket_list = useSelector((state) => state.bucket.list);
-  // url 파라미터에서 인덱스 가져오기
-  const params = useParams();
-  const bucket_index = params.index;
-  console.log(params);
+const bucket_list = useSelector((state) => state.bucket.list);
+// url 파라미터에서 인덱스 가져오기
+const params = useParams();
+const bucket_index = params.index;
+const history = useHistory();
+const dispatch = useDispatch();
 
-  return <h1>{bucket_list[bucket_index]}</h1>;
+  return <>
+            <h1>{bucket_list[bucket_index]}</h1>
+            <button onClick={() => {
+                console.log("삭제하기 버튼 누름");
+                dispatch(deleteBucket(bucket_index));
+                history.goBack();
+            }}>삭제하기</button>
+        </>
 };
 
 export default Detail;
--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -1,27 +1,48 @@
-// bucket.js
+// 액션 타입을 정해줍니다.
+const CREATE = "bucket/CREATE";
+const DELETE = "bucket/DELETE";
 
-// Actions
-const CREATE = 'bucket/CREATE';
 
+// 초기 상태값을 만들어줍니다.
 const initialState = {
-    list : ["영화관 가기", "매일 책읽기", "수영 배우기","테스트"],
-}
+  list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+};
 
-// Action Creators
-export function createBucket(bucket) {
-  console.log("액션을 생성할거야!");
-    return {type: CREATE, bucket:bucket};
-}
+// 액션 생성 함수예요.
+// // 액션을 만들어줄 함수죠!
+// export const loadBucket = (bucket) => {
+//   return { type: LOAD, bucket };
+// };
+
+export const createBucket = (bucket) => {
+  return { type: CREATE, bucket };
+};
+export const deleteBucket = (bucket_index) => {
+  console.log("지울 버킷 인덱스" , bucket_index);
+  return { type: DELETE , bucket_index };
+};
 
 
-// Reducer
+
+// 리듀서예요.
+// 실질적으로 store에 들어가 있는 데이터를 변경하는 곳이죠!
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
-    case "bucket/CREATE" : {
-        console.log("이제 값을")
-        const new_bucket_list = [...state.list, action.bucket];
-        return {list : new_bucket_list};
+    case "bucket/CREATE": {
+      const new_bucket_list = [...state.list, action.bucket];
+      return { list: new_bucket_list };
     }
-    default: return state;
+  
+    case "bucket/DELETE": {
+      console.log(state,action);
+      const new_bucket_list = state.list.filter((data, idx) => {
+          return parseInt(action.bucket_index) !== idx
+        });
+        
+        return {list: new_bucket_list};
+    }
+      
+    default:
+      return state;
   }
 }
