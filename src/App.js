22267d572d61169f3466706ef81db34170448450
JoyKim66 <masscom6051@naver.com>
2022-05-30 10:47:39 +0900

    keyframe사용해서 animation효과주기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, {keyframes} from "styled-components"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box></Box>
     </div>
   );
 }
+const boxAnimation = keyframes`
+  0%{
+    border-radius: 0px;
+    top: 20px;
+    opacity: 1;
+
+
+  }
+  30% {
+    top: 300px;
+
+  }
+  50% {
+    border-radius: 50px;
+    top: 700px;
+    opacity: 0;
+
+
+  }
+  100% {
+    border-radius: 0px;
+    top: 20px;
+    opacity: 0;
+
+  }
+`;
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background: green;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  animation: ${boxAnimation} 2s 1s infinite linear alternate;
+`;
+
+
 
 export default App;
